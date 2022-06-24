8b9254e2503477a0a187b15ab044791191a44b56
Dan <40413569+dannistangkw@users.noreply.github.com>
2022-06-24 12:12:20 +0800

    Update fetchNews.py
    clean the code

--- fetchNews.py
+++ fetchNews.py
@@ -4,9 +4,10 @@ from datetime import datetime, timedelta
 
 i = 1
 session = Session()
-public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=important'
+# public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=important'
 # public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=hot'
-# public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true'
+public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true'
+
 
 public_response = session.get(public_url)
 pubic_data = json.loads(public_response.text)["results"]
@@ -21,12 +22,11 @@ for public_new in pubic_data:
     url = public_new["url"]
 
     content.append(title)
-    ##TODO:adjust datetime format
+    #TODO: adjust datetime format
     publish_date = datetime.strptime(publish_date, "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=8)
-    content.append(publish_date)
-    content.append(url)
+    # content.append(publish_date)
+    # content.append(url)
 
-    print(f"{i}: {title}, {publish_date}")
-    print(f"{url}\n")
+    print(f"{i}: {title}, {publish_date}\n{url}\n")
 
     i += 1
