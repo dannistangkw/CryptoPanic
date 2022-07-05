1d083f2c5d799b04ed287e1f05eb3b3aff4b2a7d
Dan <40413569+dannistangkw@users.noreply.github.com>
2022-07-05 16:15:00 +0800

    add workbook lib for hyperlink embed

--- fetchNews.py
+++ fetchNews.py
@@ -1,33 +1,40 @@
-from requests import Request, Session
+from requests import Session
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from openpyxl import Workbook
 
+
+wb = Workbook()
+sheet = wb.active
+today = date.today()
 i = 1
 session = Session()
-# public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=important'
-# public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=hot'
-public_url = 'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true'
+urls = ['https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=important',
+              'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true&filter=hot',
+              'https://cryptopanic.com/api/v1/posts/?auth_token=f8d35df861317a27f9e520363e61f2e083fea36e&public=true']
 
 
-public_response = session.get(public_url)
-pubic_data = json.loads(public_response.text)["results"]
+for public_url in urls:
+    public_response = session.get(public_url)
+    pubic_data = json.loads(public_response.text)["results"]
 
-# columns = ["Title", "Published date", "url", ]
+    for public_new in pubic_data:
+        ##TODO:create content
+        content = []
+        title = public_new["title"]
+        publish_date = public_new["created_at"]
+        url = public_new["url"]
 
-for public_new in pubic_data:
-    ##TODO:create content
-    content = []
-    title = public_new["title"]
-    publish_date = public_new["created_at"]
-    url = public_new["url"]
+        content.append(title)
+        #TODO: adjust datetime format
+        # publish_date = datetime.strptime(publish_date, "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=8)
 
-    content.append(title)
-    #TODO: adjust datetime format
-    publish_date = datetime.strptime(publish_date, "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=8)
-    # content.append(publish_date)
-    # content.append(url)
+        print(f"{i}: {title}\n{url}\n")
+        # Add a hyperlink
+        sheet.cell(row=i, column=1).value = '=HYPERLINK("{}", "{}")'.format(url, title)
+        wb.save(f"files/News_{today}.xlsx")
 
-    # print(f"{i}: {title}, {publish_date}\n{url}\n")
-    print(f"{i}: {title}\n{url}\n")
+        i += 1
 
-    i += 1
+
+print(f"Done! Please find excel file News_{today}")
