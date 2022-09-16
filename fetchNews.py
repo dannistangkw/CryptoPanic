1dc208f02d43796196e3a7da44b9130140d4e987
Dan <40413569+dannistangkw@users.noreply.github.com>
2022-09-16 15:02:48 +0800

    add the block latest news

--- fetchNews.py
+++ fetchNews.py
@@ -1,8 +1,11 @@
 from requests import Session
+import requests
 import json
 from datetime import datetime, timedelta, date
 from openpyxl import Workbook
+from bs4 import BeautifulSoup
 
+#########CryptoPanic############################################################################################################################
 
 wb = Workbook()
 sheet = wb.active
@@ -30,10 +33,39 @@ for public_url in urls:
         # publish_date = datetime.strptime(publish_date, "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=8)
 
         print(f"{i}: {title}\n{url}\n")
+
+
         # Add a hyperlink
         sheet.cell(row=i, column=1).value = '=HYPERLINK("{}", "{}")'.format(url, title)
         wb.save(f"files/News_{today}.xlsx")
 
+        i += 1
+
+#######The Block Latest###########################################################################################################################################
+
+##4 pages of news = 40
+url_list = ['https://www.theblock.co/latest']
+
+for n in range(10, 40, 10):
+    url_list.append(f'https://www.theblock.co/latest?start={n}')
+
+for url in url_list:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    result_list = soup.findAll('article')
+    ##headline
+    for k in range(0, len(result_list)):
+        result = result_list[k]
+        head_msg = result.findNext('span')
+        head = head_msg['ga-event-label'][12:]
+
+        ##link
+        link_msg = result.findNext('a')
+        link = f"https://www.theblock.co{str(link_msg['href'])}"
+
+        print(f"{i}: {head}\n{link}\n")
+        sheet.cell(row=i, column=1).value = '=HYPERLINK("{}", "{}")'.format(link, head)
+        wb.save(f"files/News_{today}.xlsx")
         i += 1
 
 
